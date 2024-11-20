fc715067876a5802e4d0b61bc02d3676bc46a73f
shahzadamodassir <shahzadamodassir@gmail.com>
2024-11-20 23:46:42 +0530

    initial commit

--- src/Connection/ConnectionInterface.php
+++ src/Connection/ConnectionInterface.php
@@ -6,7 +6,6 @@ namespace Modassir\SSE\Connection;
 
 interface ConnectionInterface
 {
-  public function write($value);
   public function read();
   public function dump();
   public function export();
